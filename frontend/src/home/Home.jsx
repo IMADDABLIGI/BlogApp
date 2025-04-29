f4bb71dbbb132847771289c9cda9d4b2345a1dfc
IMADDABLIGI <imad.dabligi@gmail.com>
2025-04-29 20:20:32 +0100

    feat(master): Landing page almost Done

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './Home.css'
 import HomeHead from './HomeHead'
+import HomeBody from './HomeBody'
 
 function Home() {
     const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function Home() {
   return (
     <div className='home_ctr'>
       <HomeHead />
+      <HomeBody />
       
     </div>
   )
--- frontend/src/home/HomeHead.jsx
+++ frontend/src/home/HomeHead.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 import logo from '../assets/InnoMindsBlogs.png'
+import { NavLink } from 'react-router-dom';
 
 function HomeHead() {
   return (
@@ -10,10 +11,12 @@ function HomeHead() {
             <p className='head_name'> InnoMinds </p>
         </div>
         <div className="head_navbar">
-
+            <NavLink to="/" className="nav_link" end>Home</NavLink>
+            <NavLink to="/blogs" className="nav_link">Blogs</NavLink>
+            <NavLink to="/about" className="nav_link">About Us</NavLink>
         </div>
         <div className="head_login">
-
+            <NavLink to="/signup" className="nav_login"> Login </NavLink>
         </div>
     </div>
   )
