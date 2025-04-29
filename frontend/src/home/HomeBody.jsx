ff448d62743fe21565151ed52b5f7916fbdad82f
IMADDABLIGI <imad.dabligi@gmail.com>
2025-04-29 21:53:14 +0100

    feat(master): Fix header and paragraphe for landingPAge

--- frontend/src/home/HomeBody.jsx
+++ frontend/src/home/HomeBody.jsx
@@ -5,8 +5,8 @@ function HomeBody() {
   return (
     <div className="home_body">
       <div className="home_left">
-        <h1 className="home_title">Welcome to the Platform</h1>
-        <p className="home_subtitle">Start sharing your ideas, discover blogs, and grow with the community.</p>
+        <h1 className="home_title"> Discover. Share. Grow with InnoMinds.</h1>
+        <p className="home_subtitle">InnoMinds is a space where entrepreneurs share their stories, struggles, and lessons.</p>
       </div>
       <div className="home_right">
         <img src={bodyImg} alt="Welcome Visual" className="home_image" />
