c18918391b495eb0998696afcf4669dfde3b41e7
IMADDABLIGI <imad.dabligi@gmail.com>
2025-04-30 10:01:38 +0100

    feat(sokaina): Create contact us page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./login/Login";
 import Register from "./login/Register";
 import About from "./about/About";
 import Blogs from "./blogs/Blogs";
+import Contact from "./contact/Contact";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
   {
 	path: "/about",
 	  element: <About />
+  },
+  {
+	path: "/contact",
+	  element: <Contact />
   },
 ]);
 
