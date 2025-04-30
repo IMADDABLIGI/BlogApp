11632aa9dfd67e9f819cddf3d141e605f6262014
IMADDABLIGI <imad.dabligi@gmail.com>
2025-04-30 12:46:34 +0100

    refactor(master): add ngrok

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,20 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/auth", authRoutes);
+app.use("/api/articles", async (req, res)=>{
+    const articles = [
+        {
+            id: 1,
+            name: "John Doe",
+            role: "CEO at TechCorp",
+            pic: "/api/placeholder/48/48",  
 
+
+            date: "April 28, 2025", }]
+        
+        return res.json(articles);
+        
+        })
 // Catch-all route for invalid routes
 // app.use((req, res) => {
 // 	res.status(404).json({ message: 'Not Found' });
