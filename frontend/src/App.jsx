71453f95d83c20fe3cd556f147f9de84ad0941b2
asserhan <asseraouhanane@gmail.com>
2025-04-30 01:37:55 +0100

    fetch api form backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./home/Home";
 import Login from "./login/Login";
 import Register from "./login/Register";
+import { useState ,useEffect} from "react";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,20 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [backData, setBackData] = useState([{}]);
+  useEffect(() => {
+    fetch("http://localhost:4000/api/")
+      .then((res) => res.json())
+      .then((data => {
+        setBackData(data);
+        console.log(data);
+        console.log("Data fetched from backend");
+      }
+      ))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return <RouterProvider router={router} />;
 }
